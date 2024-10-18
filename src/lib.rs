8f85ec87c9ea4884b6c61fc7a3bf70327b1227c7
Laurence Liang <laurence.w.liang@gmail.com>
2024-10-17 23:56:08 -0400

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+use std::vec;
+
 mod llm {
     pub fn ping() -> String {
         let var: String = "ping".to_string();
         return var;
+    }
+
+    pub fn dot(a: i32, b: i32) -> i32 {
+        return a * b;
     }
 }
 
@@ -9,11 +15,14 @@ mod llm {
 mod tests {
 
     #[test]
-
     fn ping_test() {
         let x: String = crate::llm::ping();
         // println!(crate::llm::ping().to_string());
         assert_eq!(0, 0);
     }
-}
 
+    #[test]
+    fn test_dot_product() {
+        assert_eq!(crate::llm::dot(1, 1), 1);
+    }
+}
